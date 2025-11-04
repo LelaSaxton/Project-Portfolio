f2291b253680bdf4b0801a946eaa36bc63f1bf5d
Lela Saxton <94490064+coolsealion@users.noreply.github.com>
2025-11-03 23:56:03 -0500

    Add files via upload

---
+++ Rectangle Area Calculation.java
@@ -0,0 +1,18 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+	// Rectangle data
+		int length = 12;
+		int width = 9;
+		int area;
+		
+	// Equation
+	    area = length * width;
+	    
+    // Output of data
+        System.out.println("Length of Rectangle : " + length);
+        System.out.println("Width of Rectangle : " + width);
+        System.out.println("Area of Reactangle : " + area);
+	}
+}
