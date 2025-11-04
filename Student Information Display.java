512906392fcc6aa8ffc2cb71c373f6029e0376c3
Lela Saxton <94490064+coolsealion@users.noreply.github.com>
2025-11-03 23:54:52 -0500

    Add files via upload

---
+++ Student Information Display.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		//Student Data
+		int studentIDnum = 200569;
+		int ageofStudent = 19;
+		float studentFee = 21.95f;
+		char gradeofStudent = 'A';
+		
+		//Data Output 
+		System.out.println("Student ID # : " + studentIDnum);
+		System.out.println("Student Age : " + ageofStudent);
+		System.out.println("Student Fee : $" + studentFee);
+		System.out.print("Student Grade : " + gradeofStudent);
+	}
+}
